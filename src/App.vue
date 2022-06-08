4f90d67773f93f90102a3753b5b5ce68e9e777c1
xuxinhanan <775066595@qq.com>
2022-06-08 20:28:46 +0800

    fix: playerException

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,19 @@
   <m-header></m-header>
   <tab></tab>
   <router-view></router-view>
-  <!-- <player></player> -->
+  <player></player>
 </template>
 
 <script>
 import Header from "@/components/header/header";
 import Tab from "@/components/tab/tab";
-//import Player from "@/components/player/player.vue";
+import Player from "@/components/player/player.vue";
 
 export default {
   components: {
     MHeader: Header,
     Tab,
-    // Player,
+    Player,
   },
 };
 </script>
--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -5,32 +5,154 @@
         <img :src="currentSong.pic" />
       </div>
       <div class="top">
-        <div class="back">
+        <div class="back" @click="goBack">
           <i class="icon-back"></i>
         </div>
         <h1 class="title">{{ currentSong.name }}</h1>
         <h1 class="subtitle">{{ currentSong.singer }}</h1>
       </div>
+      <div class="bottom">
+        <div class="operators">
+          <div class="icon i-left">
+            <i class="icon-sequence"></i>
+          </div>
+          <div class="icon i-left" :class="disableCls">
+            <i @click="prev" class="icon-prev"></i>
+          </div>
+          <div class="icon i-center" :class="disableCls">
+            <i @click="togglePlay" :class="playIcon"></i>
+          </div>
+          <div class="icon i-right" :class="disableCls">
+            <i @click="next" class="icon-next"></i>
+          </div>
+          <div class="icon i-right">
+            <i class="icon-not-favorite"></i>
+          </div>
+        </div>
+      </div>
     </div>
-    <audio></audio>
+    <audio ref="audioRef" @pause="pause" @canplay="ready"></audio>
   </div>
 </template>
 
 <script>
 import { useStore } from "vuex";
-import { computed } from "vue";
+import { computed, watch, ref } from "vue";
 
 export default {
   // 首先要获取 currentSong, fullScreen 数据以正确渲染
   name: "player",
   setup() {
+    const audioRef = ref(null);
+    const songReady = ref(false);
+
     const store = useStore();
     const fullScreen = computed(() => store.state.fullScreen);
-    const currentSong = computed(() => store.state.currentSong);
+    const currentSong = computed(() => store.getters.currentSong);
+    const playing = computed(() => store.state.playing);
+    const currentIndex = computed(() => store.state.currentIndex);
+    const playlist = computed(() => store.state.playlist);
+
+    const playIcon = computed(() => {
+      return playing.value ? "icon-pause" : "icon-play";
+    });
+
+    const disableCls = computed(() => {
+      return songReady.value ? "" : "disable";
+    });
+
+    watch(currentSong, (newSong) => {
+      if (!newSong.id || !newSong.url) {
+        return;
+      }
+      songReady.value = false;
+      const audioEl = audioRef.value;
+      audioEl.src = newSong.url;
+      audioEl.play();
+    });
+
+    watch(playing, (newPlaying) => {
+      if (!songReady.value) {
+        return;
+      }
+      const audioEl = audioRef.value;
+      newPlaying ? audioEl.play() : audioEl.pause();
+    });
+
+    function goBack() {
+      store.commit("setFullScreen", false);
+    }
+
+    function togglePlay() {
+      if (!songReady.value) {
+        return;
+      }
+      store.commit("setPlayingState", !playing.value);
+    }
+
+    function pause() {
+      store.commit("setPlayingState", false);
+    }
+
+    function prev() {
+      const list = playlist.value;
+      if (!songReady.value || !list.length) return;
+      if (list.length === 1) {
+        loop();
+      } else {
+        let index = currentIndex.value - 1;
+        if (index === -1) {
+          index = list.length - 1;
+        }
+        store.commit("setCurrentIndex", index);
+        if (!playing.value) {
+          store.commit("setPlayingState", true);
+        }
+      }
+    }
+
+    function next() {
+      const list = playlist.value;
+      if (!songReady.value || !list.length) return;
+      if (list.length === 1) {
+        loop();
+      } else {
+        let index = currentIndex.value + 1;
+        if (index === list.length) {
+          index = 0;
+        }
+        store.commit("setCurrentIndex", index);
+        if (!playing.value) {
+          store.commit("setPlayingState", true);
+        }
+      }
+    }
+
+    function loop() {
+      const audioEl = audioRef.value;
+      audioEl.currentTime = 0;
+      audioEl.play();
+    }
+
+    function ready() {
+      if (songReady.value) {
+        return;
+      }
+      songReady.value = true;
+    }
 
     return {
+      audioRef,
       fullScreen,
       currentSong,
+      disableCls,
+      playIcon,
+      goBack,
+      togglePlay,
+      pause,
+      prev,
+      next,
+      ready,
     };
   },
 };
@@ -94,6 +216,41 @@ export default {
         text-align: center;
         font-size: $font-size-medium;
         color: $color-text;
+      }
+    }
+    .bottom {
+      position: absolute;
+      bottom: 50px;
+      width: 100%;
+      .operators {
+        display: flex;
+        align-items: center;
+        .icon {
+          flex: 1;
+          color: $color-theme;
+          &.disable {
+            color: $color-theme-d;
+          }
+          i {
+            font-size: 30px;
+          }
+        }
+        .i-left {
+          text-align: right;
+        }
+        .i-center {
+          padding: 0 20px;
+          text-align: center;
+          i {
+            font-size: 40px;
+          }
+        }
+        .i-right {
+          text-align: left;
+        }
+        .icon-favorite {
+          color: $color-sub-theme;
+        }
       }
     }
   }
